199a1dc0c8e2da5f9dc66b4d99ad8f652a65c653
NXuanTuong <tuongbabynhok@gmail.com>
2025-02-17 13:57:03 +0700

    feat: create home screen

---
+++ src/home/components/MainScreen.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MainScreen = () => {
+  return (
+    <div className="relative rounded-b-lg h-[700px] flex justify-center items-center">
+      <img
+        className="rounded-b-2xl h-[700px] object-cover shadow-[0px_-10px_30px_rgba(255,255,255,0.5)]"
+        src="/images/background_home.png"
+        alt="Background"
+      />
+    </div>
+  );
+};
+
+export default MainScreen;
---
+++ src/home/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="w-3/4 h-15 bg-[#fff] rounded-t-2xl flex flex-row justify-between items-center px-5 z-10 ">
+        <div className=" flex flex-row justify-between items-center gap-3">
+          <div className="rounded-full w-4 h-4 bg-[#458665]"></div>
+          <div className="rounded-full w-4 h-4 bg-[#ff6379]"></div>
+          <p className="text-4xl text-[#004a37] font-['Exo_2'] font-medium">
+            Sci-Learing
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center gap-3">
+          <button className="text-base flex justify-between items-center font-medium gap-3 cursor-pointer px-2 py-2 border border-[#004a37] text-[#004a37] rounded-lg transition">
+            <i class="fa-solid fa-pen-to-square"></i> Đăng Ký
+          </button>
+
+          <button className="text-base flex justify-between font-medium items-center gap-3 cursor-pointer px-2 py-2 bg-[#004a37] text-[#fff] rounded-lg  transition">
+            <i class="fa-solid fa-user"></i> Đăng Nhập
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
